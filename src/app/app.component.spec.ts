2fe3f881204c94c47f0ccc1aaa63cfa0f8829563
Naresh Kumar H <nareshkumarh@live.com>
2022-08-09 13:48:10 +0530

    added logs in appcomponent test case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
+    console.log("AppComponent - beforeEach");
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
@@ -14,19 +15,26 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  afterEach(async () => {
+    console.log("AppComponent - afterEach");
+  });
+
   it('should create the app', () => {
+    console.log("###AppComponent - Test Case 1");
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'BookApp'`, () => {
+    console.log("###AppComponent - Test Case 2");
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('BookApp');
   });
 
   it('should render title', () => {
+    console.log("###AppComponent - Test Case 3");
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
